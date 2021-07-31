c47a86600310420fc5a89140a3aaa1a3557716d5
Zoya Ivanova <ivanova.zoya.r@gmail.com>
2021-07-31 19:11:23 +0300

    R1 Message component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from "react";
+import {Message} from "./components/Message/Message";
 
 export class App extends Component {
     render() {
         return (
             <div>
-                <h1 className="bb bg-info">React2!</h1>
+                <h1 className="bg-info">React!</h1>
+                <Message message="Hi! I'm Zoya.">
+                    No messages just yet.
+                </Message>
             </div>
         );
     }
