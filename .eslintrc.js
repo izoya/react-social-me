6024f706b177b396af1b31914b6a9c27a8e38572
Zoya Ivanova <ivanova.zoya.r@gmail.com>
2021-08-01 12:06:04 +0300

    R2 Linter setup

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        'comma-dangle': ['error', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+        }],
+        'object-curly-spacing': [2, 'never'],
+        quotes: ['error', 'single'],
+        'keyword-spacing': ['error'],
+        'space-before-function-paren': ['error', {
+            anonymous: 'always',
+            named: 'never',
+            asyncArrow: 'always',
+        }],
+        'space-before-blocks': ['error', 'always'],
+        'no-multiple-empty-lines': ['error', {
+            max: 1,
+        }],
+        'no-trailing-spaces': ['error'],
+        'padding-line-between-statements': [
+            'error',
+            {
+                blankLine: 'always',
+                prev: '*',
+                next: 'return',
+            },
+        ],
+        'react/prop-types': 'off',
+        semi: [2, 'always'],
+    },
+    overrides: [
+        {
+            files: ['*.js', '*.jsx'],
+            rules: {
+                indent: ['error', 4],
+            },
+        },
+    ],
+};
